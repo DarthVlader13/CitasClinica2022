9071cab04f761d4443edff6e66f8926db17ca748
Vlader <Vlader@DESKTOP-6HKH0SF>
2022-01-20 15:17:02 +0100

    JHFA: Añado el enumerado (Enum) "Opciones".

---
+++ src/main/java/org/iesalandalus/programacion/vista/Opciones.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.vista;
+
+//Creamos las opciones para usar posteriormente
+
+public enum Opciones {
+
+	SALIR, INSERTAR_CITA, BUSCAR_CITA, BORRAR_CITA, MOSTRAR_CITAS_DIA, MOSTRAR_CITAS; 
+
+}
